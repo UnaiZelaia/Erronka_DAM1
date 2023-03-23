065a43d142f178fa7be321bcff128ade1a632e0e
Niistal <114486230+Niistal@users.noreply.github.com>
2023-03-23 14:28:02 +0100

    Add files via upload

--- employees/employees.py
+++ employees/employees.py
@@ -1,48 +1,8 @@
 from os import *
+from person import *
+
 
 class Employee:
-        def __init__(self, name, position, salary):
-            self.name = name
-            self.position = position
-            self.salary = salary
 
-class Canteen:
-        def __init__(self, file_name):
-            self.file_name = file_name
-
-        def read_file(self):
-            try:
-                with open(self.file_name, 'r') as file:
-                    lines = file.readlines()
-                    employees = []
-                    for line in lines:
-                        employee_data = line.split(',')
-                        name = employee_data[0].strip()
-                        position = employee_data[1].strip()
-                        salary = int(employee_data[2].strip())
-                        employee = Employee(name, position, salary)
-                        employees.append(employee)
-                    return employees
-            except FileNotFoundError:
-                print(f"{self.file_name} does not exist")
-
-        def write_file(self, employees):
-            with open(self.file_name, 'w') as file:
-                for employee in employees:
-                file.write(f"{employee.name}, {employee.position}, {employee.salary}\n")
-
-        def add_employee(self, employee):
-            employees = self.read_file()
-            employees.append(employee)
-            self.write_file(employees)
-
-        def edit_employee(self, old_employee, new_employee):
-            employees = self.read_file()
-            index = employees.index(old_employee)
-            employees[index] = new_employee
-            self.write_file(employees)
-
-        def delete_employee(self, employee):
-            employees = self.read_file()
-            employees.remove(employee)
-            self.write_file(employees)
+    def __init__(self, file_name):
+       super 
---
+++ employees/person.py
@@ -0,0 +1,26 @@
+
+class person:
+    def __init__(self, name, position, salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+    # getter method
+
+    def get_name(name):
+        return self.name
+
+    def get_position(position):
+        return self.position
+
+    def get_salary(salary):
+        return self.salary
+
+    # setter method
+    def set_name(self, nm):
+        self.name = x
+
+    def set_position(self, po):
+        self.position = y
+
+    def set_salary(self, sa):
+        self.salary = sa
